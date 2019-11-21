84d0e5de1a739543bcdc2c10dfaa28a230eb95c9
Vinaya Bhat <vibha2@pdx.edu>
2019-11-20 16:31:27 -0800

    A5

---
+++ assignment#5/app.js
@@ -0,0 +1,21 @@
+const expressobj = require('express');
+const appobject = expressobj(); // Appobject has many methods
+const path = require('path');
+const bodyparser = require('body-parser');
+
+appobject.use(bodyparser.urlencoded({ extended: false }));
+
+appobject.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+appobject.post('/submit', (req, res) => {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write("Name : "+req.body.Name +"\n");
+    res.write("Email : "+req.body.Email +"\n");
+    res.write("Comment : "+req.body.Comment+"\n");
+    res.end();
+});
+
+appobject.listen(8080);
+
